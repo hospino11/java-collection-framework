defabcd3724180e708fa0b5c492226ff8a9a2b83
Hansel Junior Ospino Orellano <hansel.ospino@globant.com>
2020-03-14 15:10:26 -0500

    feature - arraylist add at index: ArrayList add(int, Object) method example.

--- src/main/java/com/jcf/features/arraylist/ArrayListAddExample.java
+++ src/main/java/com/jcf/features/arraylist/ArrayListAddExample.java
@@ -25,5 +25,15 @@ public class ArrayListAddExample {
         scores.add(99);
         scores.add(78);
         log.log(Level.INFO, "Scores added by add method: {0}", scores);
+
+        List<String> greetings = new ArrayList<>();
+        greetings.add("Hi");
+        greetings.add("Hello");
+        greetings.add("Hey");
+        greetings.add("Whats up");
+        greetings.add(2, "Howdy");
+        log.log(Level.INFO, "Greetings added by add(index, object): {0}", greetings);
+        greetings.add(0, "Bye");
+        log.log(Level.INFO, "Bye added to greetings at index 0: {0}", greetings);
     }
 }
